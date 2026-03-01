246eef9d4d68e7e3fec0cde7d1ea48a93320d33f
christofvb <christof.vanberendonck@hotmail.com>
2026-03-01 07:27:50 +0100

    Register network Stimulus controller in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,7 @@ import TinyMCEController from "controllers/tinymce_controller"
 import ProjectsController from "controllers/projects_controller"
 import ModalController from "controllers/modal_controller"
 import TypingController from "controllers/typing_controller"
+import NetworkController from "controllers/network_controller"
 
 const application = Application.start()
 window.Stimulus = application
@@ -13,3 +14,4 @@ application.register("tinymce", TinyMCEController)
 application.register("projects", ProjectsController)
 application.register("modal", ModalController)
 application.register("typing", TypingController)
+application.register("network", NetworkController)
